0f366f65cc97f5373e7b5fb4abe340481c08d171
AndreyBeloborodov <andreybelo73.30@gmail.com>
2022-03-03 17:26:50 +0300

    first problem litcode

---
+++ src/AISD/Litcode/N1.java
@@ -0,0 +1,30 @@
+package AISD.Litcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class N1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        int ns = s.length(), nt = t.length();
+        int[][] dp = new int[ns + 1][26];
+        for (int i = 1; i < ns + 1; i++) {
+            for (int j = 0; j < 26; j++) {
+                dp[i][j] = dp[i - 1][j] + (s.charAt(i - 1) == ('a' + j) ? 1 : 0);
+            }
+        }
+        int[] cur = new int[26];
+        for (int i = 0; i < nt; i++) cur[t.charAt(i) - 'a']++;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < ns - nt + 1; i++) {
+            boolean ok = true;
+            for (int j = 0; j < 26 && ok; j++) ok &= (dp[i + nt][j] - dp[i][j] == cur[j]);
+            if (ok) ans.add(i);
+        }
+        for (Integer c : ans) System.out.print(c + " ");
+
+    }
+}
